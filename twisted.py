342844e4f929c2e73bc5eb86a317b7760cbeaeac
lawrencfgsdfg <64938169+lawrencfgsdfg@users.noreply.github.com>
2020-11-15 01:54:43 -0500

    v0.2

--- twisted.py
+++ twisted.py
@@ -1,211 +1,126 @@
-# import pyTwistyScrambler
-# from pyTwistyScrambler import scrambler222
 import time
 import sys
 import random
+import os
+import math
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
-# discord rpc init, and prevent crash
-discordRunning = "false"
+# WOW functions are inefficient. i'm a lazy person i'll fix it later
 
-try:
-    import pygame
-    pygame.init()
-except:
-    print("pygame fucked")
+try: from scrambler import *
+except: print("> scrambler.py not found\n")
 
-# here for when the module just decides not to fucking work and i have to debug shit and not wait for RPC to work
-# basically, make True for when you want discord, False if you don't care
-if True:
+try: import pygame; pygame.init()
+except: input("> pygame not installed, enter to exit\n")
+
+# you can change this to disable RPC attempts.
+discordRunning = False;
+if False:
     try:
         from pypresence import Presence
-        client_id = '770513436735569940' 
-        RPC = Presence(client_id)
+        RPC = Presence('770513436735569940' )
+        print("> trying rpc...")
         RPC.connect()
-        print("discord rpc running")
-        discordRunning = "true"
-    except:
-        print("pypresence fucked")
-        discordRunning = "false"
+        discordRunning = True
+    except: print("> rpc failed.")
 
-# screen shit
-screen = pygame.display.set_mode((640, 360))
-clock = pygame.time.Clock()
-
-# done is running var, also version shit here
-done = False
-version = "v0.1"
-pygame.display.set_caption('twisted ' + version)
-
-font = pygame.font.SysFont("Arial", 16)
-
-# render text... well, no shit
-def rendertext(t, xpos, ypos, size, center, r, g, b, centerypos):
-    font = pygame.font.SysFont("Arial", size)
-    text = font.render(t, True, (r, g, b))
+# render text obviously
+def rendertext(t, xpos, ypos, size, center, hexcolor, dobold, doitalic, doblit, r=-1, g=-1, b=-1):
+    font = pygame.font.SysFont("arial", size, bold=dobold, italic=doitalic)
+    text = font.render(t, True, pygame.Color(hexcolor))
+    if r > 0 or g > 0 or b > 0: text = font.render(t, True, (r, g, b))
     text_rect = text.get_rect(center=(320, 180))
-    if not center:
-        screen.blit(text,(xpos, ypos))
+    if center == False: screen.blit(text,(xpos, ypos))
     else:
-        if centerypos > 0:
-            text_rect = text.get_rect(center=(320, centerypos))
-            screen.blit(text,text_rect)
+        if center == True: screen.blit(text,text_rect)
         else:
+            text_rect = text.get_rect(center=(320, center))
             screen.blit(text,text_rect)
-    pygame.display.flip()
+    clock.tick(100)
+    if doblit: pygame.display.flip()
 
-def renderrect(xpos, ypos, width, height):
+def renderrect(xpos, ypos, width, height, doblit, hexcolor, center):
     rect = pygame.Rect(xpos, ypos, width, height)
-    pygame.draw.rect(screen, (0, 0, 0), rect)
-    pygame.display.flip()
-    
+    if center: rect.center = (320, 180)
+    pygame.draw.rect(screen, pygame.Color(hexcolor), rect)
+    if doblit: pygame.display.flip()
 
 # update rpc
-def doRPC(d, s):
-    try:
-        RPC.update(details=d, state=s, large_image="thing", large_text=version + "; by lawrencfgsdfg")
-    except:
-        print("tried updating RPC, failed")
-
-
-screen.fill((0, 0, 0))
-rendertext("twisted " + version, 4, 4, 16, False, 75, 75, 75, 0)
-rendertext("twisted " + version, 3, 3, 16, False, 255, 255, 255, 0)
-rendertext("lawrencfgsdfg", 4, 21, 10, False, 75, 75, 75, 0)
-rendertext("lawrencfgsdfg", 3, 20, 10, False, 255, 255, 255, 0)
-
-doRPC(version, "testing")
-# rpc update
-
-finalscramble = ""
-active = 0
+def doRPC(d, s):    
+    try: RPC.update(details=d, state=s, large_image="thing", large_text=version + "; by lawrencfgsdfg")
+    except: return
 
 def waitSpaceUp():
     global starting
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
             if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
-                doRPC("solving", finalscramble)
+                doRPC("solving", currentscr)
                 starting = time.time()
                 print("space up, timer start")
-                renderrect(0, 70, 620, 200)
-                rendertext("solve", 0, 0, 48, True, 255, 255, 255, 0)
                 active = 1
                 waitSpaceTime()
-                
+            if event.type == pygame.QUIT: RPC.close(); pygame.quit(); sys.exit();
 
 def waitSpace():
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                # second space
-                return
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE: return
+            if event.type == pygame.QUIT: RPC.close(); pygame.quit(); sys.exit();
+        renderrect(0, 0, 200, 70, False, '#0e0e1c', True)
+        rendertext(str(format(time.time() - starting, '.2f')), 0, 0, 48, True, '#8096c8', False, False, True) # abs(math.cos(float(format(time.time() - starting, '.2f')))*128), 120, 200
 
 def waitSpaceTime():
-    global active
-    global ending
-    # check timer state
-    if active == 1:
-        # timer state was running, now is stopped.
+    global active, ending, timelog # check timer state here
+    if active == 1: # timer was running, now stopped
         waitSpace()
         active = 0
         ending = time.time()
-        timed = round((ending - starting)/0.01)*0.01
-        print("second space, timer stop, solve " + str(timed))
-        doRPC("solved in " + str(timed), finalscramble)
-        renderrect(145, 70, 350, 200)
-        rendertext(str(timed), 0, 0, 48, True, 255, 255, 255, 0)
-        getScramble()
+        timed = format(ending - starting, '.2f')
+        doRPC("solved in " + str(timed), currentscr)
+        renderrect(0, 0, 200, 70, False, '#0e0e1c', True)
+        rendertext(str(timed), 0, 0, 48, True, '#8096c8', False, False, True, 128, 150, 200)
+        scramble()
         waitSpaceTime()
-    else:
-        # timer state was stopped, now is running.
+    else: # timer was stopped, now waiting
         active = 1
         while True:
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                    rect = pygame.Rect(0, 70, 640, 200)
-                    pygame.draw.rect(screen, (0, 0, 0), rect)
-                    rendertext("waiting", 0, 0, 48, True, 255, 255, 255, 0)
+                    renderrect(0, 0, 200, 70, False, '#0e0e1c', True)
+                    rendertext("waiting", 0, 0, 48, True, '#8096c8', False, False, True)
                     print("initial space down, waiting")
-                    waitSpaceUp()
+                    waitSpaceUp() # start timing
                     return
+                if event.type == pygame.QUIT: RPC.close(); pygame.quit(); sys.exit();
 
-OLDSCRAMBLE = ""
-finalscramble = ""
-def getScramble():
-    # don't ask me how the FUCK this works
-    print("\n")
-    global OLDSCRAMBLE
-    global finalscramble
-    
-    renderrect(0, 290, 640, 50)
-    
-    char = ["F", "R", "U"]
-    char2 = ["B", "L", "D"]
-    scramble = []
-    currentchar = ""
-    lastchar = "temp"
-    
-    i = 0
-    while i < 6:
-        rand = random.randint(1, 3)
-        currentchar = random.randint(0, 2)
-        while currentchar == lastchar:
-            print("currentchar is same as last one")
-            currentchar = random.randint(0, 2)
-        lastchar = currentchar
-        
-        if rand == 1:
-            currentchar = char[currentchar] + "'"
-        elif rand == 2:
-            currentchar = char[currentchar] + "2"
-        else:
-            currentchar = char[currentchar]
-        print(currentchar + " appended")
-        scramble.append(currentchar)
-        
-        rand = random.randint(1, 3)
-        currentchar = random.randint(0, 2)
-        while currentchar == lastchar:
-            print("currentchar is same as last one")
-            currentchar = random.randint(0, 2)
-        lastchar = currentchar
-    
-        if rand == 1:
-            currentchar = char2[currentchar] + "'"
-        elif rand == 2:
-            currentchar = char2[currentchar] + "2"
-        else:
-            currentchar = char2[currentchar]
-        print(currentchar + " appended")
-        scramble.append(currentchar)
-        
-        i = i + 1
-    
-    print("\n\nfinal generated scramble: " + ' '.join(scramble))
-    OLDSCRAMBLE = finalscramble
-    finalscramble = (' '.join(scramble))
-    
-    rendertext("current: " + str(finalscramble), 0, 100, 18, True, 255, 255, 255, 300)
-    
-    if not OLDSCRAMBLE == "":
-        rendertext("old: " + str(OLDSCRAMBLE), 0, 300, 18, True, 100, 100, 100, 320)
+def scramble():
+    global currentscr
+    # renderrect(0, 290, 290, 40, False, 'black', True)
+    renderrect(170, 270, 300, 40, False, '#0e0e1c', False)
+    try:
+        oldscr = currentscr
+        currentscr = getScramble(10)
+        rendertext("current: " + currentscr, 0, 0, 17, 280, '#8096c8', False, False, True)
+        if not oldscr == "": rendertext("old: " + oldscr, 0, 0, 17, 300, '#205252', False, False, True)
+    except: rendertext("scrambler.py missing", 0, 0, 17, 290, '#ff6464', False, False, True)
 
-rendertext("ready", 0, 0, 48, True, 255, 255, 255, 0)
+# basic init
+screen = pygame.display.set_mode((640, 360))
+clock = pygame.time.Clock()
+version = "v0.2"
+pygame.display.set_caption('twisted ' + version)
+currentscr = "none"
+active = 0
 
-running = 0
-startTime = "bruh"
-finalTime = "bruh"
-i = 0
+screen.fill((23, 23, 46))
+renderrect(175, 275, 300, 40, False, '#05050a', False) # scramble
+renderrect(225, 150, 200, 70, False, '#05050a', False) # ready shadow
+renderrect(0, 0, 200, 70, False, '#0e0e1c', True) # ready
+rendertext("ready", 0, 0, 48, True, '#8096c8', 0, False, False)
 
-pygame.display.flip()
-getScramble()
+doRPC(version, "testing")
+rendertext("twisted " + version, 2, 1, 12, False, '#27274f', False, False, False)
+rendertext("discord rpc running: " + str(discordRunning), 2, 347, 10, False, '#0e0e1c', 0, False, False)
+scramble()
 waitSpaceTime()
